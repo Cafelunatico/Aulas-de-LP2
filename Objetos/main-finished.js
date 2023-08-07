ef607f6a4840eb771e31cf8fba5286d99b38d3b1
Cafelunatico <141651226+Cafelunatico@users.noreply.github.com>
2023-08-07 11:38:50 -0400

    Add files via upload

---
+++ Objetos/main-finished.js
@@ -0,0 +1,16 @@
+//Set up canvas
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
+
+const width = canvas.width = window.innerWidth;
+const height = canvas.height = window.innerHeight;
+
+//Function to generate random number
+function random(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+//Funtion to generate random color
+function randomRGB() {
+    return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
+}
